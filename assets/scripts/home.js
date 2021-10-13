d55e148c377ac23d4e0bbb114eb2e9c4a494b7a1
Putra Krihshna <krishna.putra332@gmail.com>
2021-10-13 22:06:48 +0700

    add scroll to top button

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,1 +1,26 @@
-console.log('Home');
+// constantas
+const REM = 16;
+
+// html
+const body = document.body;
+const header = body.querySelector('header');
+const sections = body.querySelectorAll('main.home-sections-wrapper section');
+const scrollTopButton = body.querySelector('button[title=\'scroll-top-button\']');
+const limitBeforeScrollTopButtonAppear = sections[0].getBoundingClientRect().height;
+
+if (scrollTopButton != null) {
+  scrollTopButton.onclick = function () {
+    header.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
+  }
+}
+
+// scroll listener
+function scrollListener() {
+  if (window.scrollY >= limitBeforeScrollTopButtonAppear) {
+    return scrollTopButton.classList.replace('button-invisible', 'button-visible');
+  }
+
+  return scrollTopButton.classList.replace('button-visible', 'button-invisible');
+}
+
+window.addEventListener('scroll', scrollListener);
